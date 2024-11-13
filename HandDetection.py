a89541b1fd9c49e8ca4ab57909f1c13d958ded6c
Charanjit <charanjitnandigama24@gmail.com>
2024-11-13 22:26:47 +0530

    added

--- HandDetection.py
+++ HandDetection.py
@@ -1,8 +1,7 @@
 import mediapipe as mp
 import cv2
 
-class DetectHand:
-
+class HandDetection:
     def __init__(self, staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5):
         self.staticMode = staticMode
         self.maxHands = maxHands
@@ -17,12 +16,8 @@ class DetectHand:
                                         min_tracking_confidence=self.minTrackCon)
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
-        self.fingers = []
-        self.lmList = []
 
-
-    
-    def findHands(self, frame, draw=True, flip = True):
+    def scan_hands(self, frame, draw=True, flip = True):
         
         frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(frameRGB)
@@ -75,59 +70,41 @@ class DetectHand:
         return allHands, frame
     
 
-    def upwardFingers(self, hand):
+    def upward_fingers(self, hand):
 
         fingers = []
         handType = hand["type"]
         landmarksList = hand["LM_List"]
         if self.results.multi_hand_landmarks:
 
-            # For Thumb Finger
             if handType == "Right":
+                fingers.append("R")
+                # For Thumb Finger
                 if landmarksList[self.tipIds[0]][0] < landmarksList[self.tipIds[0] - 1][0]:
                     fingers.append(1)
                 else:
                     fingers.append(0)
-            else:
+                
+                # For four other Fingers of right hand
+                for id in range(1, 5):
+                    if landmarksList[self.tipIds[id]][1] < landmarksList[self.tipIds[id] - 2][1]:
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+
+            else:         # left hand
+
+                fingers.append("L")
+                # For four Fingers of left hand except thumb
+                for id in range(4,0,-1):
+                    if landmarksList[self.tipIds[id]][1] < landmarksList[self.tipIds[id] - 2][1]:
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+                # For thumb finger
                 if landmarksList[self.tipIds[0]][0] > landmarksList[self.tipIds[0] - 1][0]:
                     fingers.append(1)
                 else:
                     fingers.append(0)
 
-            # For four other Fingers
-            for id in range(1, 5):
-                if landmarksList[self.tipIds[id]][1] < landmarksList[self.tipIds[id] - 2][1]:
-                    fingers.append(1)
-                else:
-                    fingers.append(0)
         return fingers
-
-
-def main():
-
-    cap = cv2.VideoCapture(0)
-    detector = DetectHand(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
-
-    while True:
-        ret_bool, frame = cap.read()
-        frame = cv2.flip(frame,1)
-
-        hands, frame = detector.findHands(frame, draw=True,flip=True)
-
-        if hands:
-            hand1 = hands[0]
-            landmarksList1 = hand1["LM_List"]
-            box1 = hand1["bbox"]
-            center1 = hand1["center"]
-            handType1 = hand1["type"]
-            
-            upFingers1 = detector.upwardFingers(hand1)
-            print(f"---->{upFingers1}")
-
-        cv2.imshow('Frame', frame)
-        if cv2.waitKey(1) == 27 :
-            break
-
-            
-if __name__ == "__main__":
-    main()
